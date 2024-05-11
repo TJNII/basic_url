e348c6ff764a342f134bc19634023ee2116349a3
Tom Noonan II <tom@tjnii.com>
2024-05-11 00:32:44 -0400

    [CI/CD] Fix push script

--- cicd/push_gem.sh
+++ cicd/push_gem.sh
@@ -6,5 +6,8 @@ set -euo pipefail
 out_version=$(./cicd/current_gem_version.sh)
 out_file="out/basic_url.$(./cicd/current_gem_version.sh).gem"
 
-gem push --otp "${out_file}"
+echo "Enter Rubygems OTP code"
+read otp
+
+gem push "${out_file}" --otp "${otp}"
 git tag "${out_version}"
